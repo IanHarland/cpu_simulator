35123742d5b0d76ea65f4e88d7012c6fba96db39
Ian Harland <harlandvibes@gmail.com>
2024-01-29 11:25:19 -0800

    Build half_adder, full_adder, and tests

--- adder.py
+++ adder.py
@@ -5,9 +5,25 @@ def half_adder(i, j):
     carry = and_(i, j)
     return result, carry
 
+def full_adder(i, j, c):
+    carry = or_(and_(or_(i, j), c), and_(i, j))
+    result = or_(or_(and_(xor_(i,j), not_(c)), and_(nor_(i,j), c)), and_(and_(i,j), c))
+    return result, carry
+
+
 #tests
 if __name__ == '__main__':
+    print("Half adder tests:")
     print ('(result, carry)', half_adder(0, 0))
     print ('(result, carry)', half_adder(0, 1))
     print ('(result, carry)', half_adder(1, 0))
     print ('(result, carry)', half_adder(1, 1))
+    print ('/n Full adder tests:')
+    print ('(result, carry)', full_adder(0, 0, 0))
+    print ('(result, carry)', full_adder(1, 0, 0))
+    print ('(result, carry)', full_adder(0, 1, 0))
+    print ('(result, carry)', full_adder(0, 0, 1))
+    print ('(result, carry)', full_adder(1, 1, 0))
+    print ('(result, carry)', full_adder(0, 1, 1))
+    print ('(result, carry)', full_adder(1, 0, 1))
+    print ('(result, carry)', full_adder(1, 1, 1))
