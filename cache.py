2bd970911895172ce23736930fbe3fa50ae92bc4
Ian Harland <harlandvibes@gmail.com>
2024-01-31 23:27:32 -0800

    Update get method in cache

--- cache.py
+++ cache.py
@@ -1,19 +1,35 @@
 class Cache:
 
     def __init__(self):
-        ##stores memory location and instruction
-        self.locations = [('', '') for i in range(2**5)]
+        ##stores memory location [0] and data [1] in list
+        self.j = 0
+        cache_register = ['', '']
+        for i in range(32):
+            cache_register[0] += '0'
+            cache_register[1] += '0'
+        self.locations = [cache_register for i in range(128)]
     
     def __repr__(self):
         return f"Current status of cache:\n{self.locations}"
     
-    def get(self, memory_name, memory_location):
-        instruction = memory_name.locations[memory_location]
+    def get(self, memory_name, memory_register):
+        for i in range(128):
+            if memory_register == self.locations[i][0]:
+                print("Cache hit!")
+                return self.locations[i][1]
+        data = memory_name.locations[memory_register]
+        empty_data = ''
+        for a in range(32):
+            empty_data += '0'
         i = 0
         current = self.locations[i]
-        while current[0] != 0:
+        while current[0] != empty_data:
             i += 1
-            if i == 32:
-                current = self.locations[0]
+            if i == 128:
+                current = self.locations[j]
+                self.j += 1
+                if self.j == 128:
+                    self.j = 0
                 break
-        current = (memory_location, instruction)
+        current = [memory_register, data]
+        return data
